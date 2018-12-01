e8d4ec06519cbe8cddbcba3ed5eed09c504a05b1
mk <1187364874@qq.com>
2018-12-01 16:35:11 +0800

    搭建页面框架

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+class AppColors {
+  static const AppBarColor = 0xff303030;
+  static const TabIconActive=0xff46c11b;
+  static const TabIconNormal=0xff999999;
+  static const AppBarPopMenuTextColor=0xffffffff;
+}
+
+class Constants {
+  static const IconFontFamily="appIconFont";
+}
